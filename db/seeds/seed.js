a8f9b5b7827cdfe9bdc7b54e50a7ae9bb9d3745b
CharmingBeetle <nina.e.thomas@gmail.com>
2025-02-28 14:00:49 +0000

    feat. added all tables and passed all tests

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,14 +3,14 @@ const articles = require("../data/test-data/articles");
 const users = require("../data/test-data/index")
 const format = require("pg-format");
 const topics = require("../data/test-data/index")
-
+const {convertTimestampToDate} = require("./utils")
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
   console.log('invoking seed...')
   return db
-  .query("DROP TABLE IF EXISTS articles;")
+  .query("DROP TABLE IF EXISTS comments;")
   .then(() => {
-    return db.query("DROP TABLE IF EXISTS comments;");
+    return db.query("DROP TABLE IF EXISTS articles;");
   })
   .then(() => {
     return db.query("DROP TABLE IF EXISTS users;");
@@ -86,53 +86,68 @@ function createTopics(topics) {
 }
   function createArticles(articles) {
     return db.query(
-      // `DROP TABLE IF EXISTS articles;
       `CREATE TABLE articles (
       article_id SERIAL PRIMARY KEY,
       title VARCHAR(255) NOT NULL,
       topic VARCHAR(255) NOT NULL,
-      author VARCHAR(255)) NOT NULL, 
+      author VARCHAR(255) NOT NULL, 
       body TEXT NOT NULL, 
       created_at TIMESTAMP NOT NULL, 
       votes INT DEFAULT 0, 
-      article_img_url VARCHAR(1000))`
-    )
+      article_img_url VARCHAR(1000)
+    );
+    `)
     .then(() => {
       const formattedArticles = articles.map((article) => {
+       const formatArticle = convertTimestampToDate(article)
+       console.log(formatArticle, "<<<<< formatted timestamp")
       return [
-      article.article_id,
-      article.title, 
-      article.topic, 
-      article.author, 
-      article.body, 
-      article.created_at, 
-      article.votes, 
-      article.article_img_url
+      formatArticle.title, 
+      formatArticle.topic, 
+      formatArticle.author, 
+      formatArticle.body, 
+      formatArticle.created_at,
+      formatArticle.votes, 
+      formatArticle.article_img_url
     ]})
   
   console.log(formattedArticles, "<<<<< FORMATTED TOPICS")
-      const insertTopicsQuery = format(
-        `INSERT INTO topics (slug, description, img_url) VALUES %L RETURNING*`,
-        formattedTopics);
-        console.log(insertTopicsQuery)
-        return db.query(insertTopicsQuery)
+      const insertArticlesQuery = format(
+        `INSERT INTO articles (title,topic,author,body,created_at,votes, article_img_url) VALUES %L RETURNING*`,
+        formattedArticles);
+        console.log(insertArticlesQuery)
+        return db.query(insertArticlesQuery)
   });
     }
-  //   function createComments() {
-  //     return db.query(
-  //     // `DROP TABLE IF EXISTS comments;
-  //       `CREATE TABLE comments 
-  //       (comment_id SERIAL PRIMARY KEY NOT NULL,
-  //       article_id INT 
-  //       FOREIGN KEY (article_id) REFERENCES article(article_id),
-  //       body VARCHAR(255) NOT NULL,
-  //       votes INT DEFAULT 0,
-  //       author VARCHAR(255)
-  //       FOREIGN KEY (author) REFERENCES user(username), 
-  //       body TEXT NOT NULL, 
-  //       created_at TIMESTAMP NOT NULL)
-  //       `
-  //       )
-  //     }
+    function createComments(comments) {
+      return db.query(
+      `CREATE TABLE comments (
+        comment_id SERIAL PRIMARY KEY NOT NULL,
+        article_id INT,
+        FOREIGN KEY (article_id) REFERENCES articles(article_id),
+        body TEXT NOT NULL,
+        votes INT DEFAULT 0,
+        author VARCHAR(255),
+        FOREIGN KEY(author) REFERENCES users(username), 
+        created_at TIMESTAMP NOT NULL);
+        `)
+        .then(() => {
+          const formattedComments = comments.map((comment) => {
+           const formatComment = convertTimestampToDate(comment)
+           console.log(formatComment, "<<<<< formatted timestamp")
+          return [
+          formatComment.article_id, 
+          formatComment.body, 
+          formatComment.votes, 
+          formatComment.author, 
+          formatComment.created_at 
+        ]})
+      
+          const insertCommentsQuery = format(
+            `INSERT INTO comments (article_id, body, votes, author, created_at) VALUES %L RETURNING*`,
+            formattedComments);
+            return db.query(insertCommentsQuery)
+      });
+      }
 
 module.exports = seed;
