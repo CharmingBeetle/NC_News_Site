6da72265802d3ad49c0a82a806b4998d9dad7dc6
CharmingBeetle <nina.e.thomas@gmail.com>
2025-03-10 15:00:42 +0000

    fix: applied review advice from task 2 and 3

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const { getEndpoints } = require("./controllers/endpoints.controller")
 const { handleServerErrors, handlePsqlErrors, handleCustomErrors } = require("./controllers/errors.controller")
 const { getTopics } = require("./controllers/topics.controller")
 const { getArticleById } = require("./controllers/articles.controller")
-app.use(express.json())
 
 
 //GET ENDPOINTS
