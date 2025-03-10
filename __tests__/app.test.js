3b626519c023313e8b7c4ca7926b4e5fad93ddc9
CharmingBeetle <nina.e.thomas@gmail.com>
2025-03-10 19:19:18 +0000

    feat: task 4 completed get articles with comment count sort/order

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,6 +5,7 @@ const request = require("supertest");
 const seed = require("../db/seeds/seed");
 const data = require("../db/data/test-data");
 const topics = require("../db/data/test-data/topics");
+const sorted = require("jest-sorted");
 
 beforeEach(() => {
   return seed(data);
@@ -38,51 +39,82 @@ describe("GET /api", () => {
         });
     });
   });
-  describe("GET: /api/articles/:article_id", () => {
-    test("200: Responds with an article object when passed an article id.", () => {
+  describe("GET: /api/articles", () => {
+    test("200: Responds with an array of all articles with total comment count for each article ID.", () => {
       return request(app)
-        .get("/api/articles/2")
+        .get("/api/articles?sort_by=created_at&order=desc")
         .expect(200)
-        .then((response) => {
-          const article = response.body.article;
-          expect(article.article_id).toBe(2);
-          expect(article.title).toBe("Sony Vaio; or, The Laptop");
-          expect(article.topic).toBe("mitch");
-          expect(article.author).toBe("icellusedkars");
-          expect(article.body).toBe("Call me Mitchell. Some years ago..");
-          expect(article.created_at).toBe("2020-10-16T05:03:00.000Z");
-          expect(article.votes).toBe(0);
-          expect(article.article_img_url).toBe(
-            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
-          );
+        .then(({ body }) => {
+          const articles = body.articles;
+          expect(articles.length).toBe(13);
+          expect(articles).toBeSortedBy("created_at", { descending: true });
+          articles.forEach((article) => {
+            expect(article.comment_count).toBeGreaterThanOrEqual(0);
+            expect(typeof article.article_id).toBe("number");
+            expect(typeof article.title).toBe("string");
+            expect(typeof article.topic).toBe("string");
+            expect(typeof article.author).toBe("string");
+            expect(typeof article.created_at).toBe("string");
+            expect(typeof article.votes).toBe("number");
+            expect(typeof article.article_img_url).toBe("string");
+            expect(typeof article.comment_count).toBe("number");
+          });
         });
     });
-    test("400: Responds with error if article ID is not valid", () => {
+    test("400: Responds with error if query entered is not valid", () => {
       return request(app)
-        .get("/api/articles/notValidId")
+        .get("/api/articles/notValidQuery")
         .expect(400)
         .then(({ body }) => {
           expect(body.msg).toBe("Bad request");
         });
     });
-    test("404: Responds with error if article does not exist", () => {
+  });
+    describe("GET: /api/articles/:article_id", () => {
+      test("200: Responds with an individual article object when passed an article id.", () => {
+        return request(app)
+          .get("/api/articles/2")
+          .expect(200)
+          .then((response) => {
+            const article = response.body.article;
+            expect(article.article_id).toBe(2);
+            expect(article.title).toBe("Sony Vaio; or, The Laptop");
+            expect(article.topic).toBe("mitch");
+            expect(article.author).toBe("icellusedkars");
+            expect(article.body).toBe("Call me Mitchell. Some years ago..");
+            expect(article.created_at).toBe("2020-10-16T05:03:00.000Z");
+            expect(article.votes).toBe(0);
+            expect(article.article_img_url).toBe(
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+            );
+          });
+      });
+      test("400: Responds with error if article ID is not valid", () => {
+        return request(app)
+          .get("/api/articles/notValidId")
+          .expect(400)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Bad request");
+          });
+      });
+      test("404: Responds with error if article does not exist", () => {
+        return request(app)
+          .get("/api/articles/99")
+          .expect(404)
+          .then(({ body }) => {
+            expect(body.msg).toBe("Article not found!");
+          });
+      });
+    });
+  });
+
+  describe("ANY: /notpath", () => {
+    test("404: Responds with error if path not found", () => {
       return request(app)
-        .get("/api/articles/99")
+        .get("/notpath")
         .expect(404)
         .then(({ body }) => {
-          expect(body.msg).toBe("Article not found!");
+          expect(body.msg).toBe("Path not found");
         });
     });
   });
-});
-
-describe("ANY: /notpath", () => {
-  test("404: Responds with error if path not found", () => {
-    return request(app)
-      .get("/notpath")
-      .expect(404)
-      .then(({ body }) => {
-        expect(body.msg).toBe("Path not found");
-      });
-  });
-});
--- models/articles.model.js
+++ models/articles.model.js
@@ -10,3 +10,41 @@ exports.fetchArticleById = (article_id) => {
     return rows[0]
   })
 }
+
+exports.fetchArticles = (sort_by='created_at', order='desc') => {
+    const validSorts = ['title', 'author', 'article_id', 'topic', 'created_at', 'votes', 'article_img_url']
+    const validOrder = ['asc', 'desc']
+        
+    if (!validSorts.includes(sort_by)) {
+        return Promise.reject({status:400, msg: "Invalid sort input"})
+    }
+    if(!order || !validOrder.includes(order)) {
+        return Promise.reject({status: 400, msg: "Invalid order"})
+    }
+        let sqlQuery= `
+        SELECT 
+        articles.author,
+        articles.title,
+        articles.article_id,
+        articles.topic,
+        articles.created_at,
+        articles.votes,
+        articles.article_img_url,
+        CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count
+        FROM 
+        articles
+        LEFT JOIN 
+        comments ON articles.article_id = comments.article_id
+        GROUP BY 
+        articles.article_id `
+
+        if(sort_by && validSorts.includes(sort_by)) {
+            sqlQuery += ` ORDER BY ${sort_by} ${order.toLowerCase()}`
+        }
+        console.log(sqlQuery, "<<<< SQL query")
+    return db.query(sqlQuery)
+    .then(({ rows })=> {
+        return rows
+    })
+}
+
