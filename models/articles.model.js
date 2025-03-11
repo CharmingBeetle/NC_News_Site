63c7768265b4ebab46b447d097062db9a4a2b220
CharmingBeetle <nina.e.thomas@gmail.com>
2025-03-11 09:56:55 +0000

    feat: task 5 get comments by article id

--- models/articles.model.js
+++ models/articles.model.js
@@ -1,27 +1,35 @@
-const db = require("../db/connection")
-
+const db = require("../db/connection");
 
 exports.fetchArticleById = (article_id) => {
-  return db.query(`SELECT * FROM articles WHERE article_id =$1`, [article_id])
-  .then(({ rows })=> {
-    if(!rows.length) {
-        return Promise.reject({status: 404, msg: "Article not found!"})
-    }
-    return rows[0]
-  })
-}
+  return db
+    .query(`SELECT * FROM articles WHERE article_id =$1`, [article_id])
+    .then(({ rows }) => {
+      if (!rows.length) {
+        return Promise.reject({ status: 404, msg: "Article not found!" });
+      }
+      return rows[0];
+    });
+};
 
-exports.fetchArticles = (sort_by='created_at', order='desc') => {
-    const validSorts = ['title', 'author', 'article_id', 'topic', 'created_at', 'votes', 'article_img_url']
-    const validOrder = ['asc', 'desc']
-        
-    if (!validSorts.includes(sort_by)) {
-        return Promise.reject({status:400, msg: "Invalid sort input"})
-    }
-    if(!order || !validOrder.includes(order)) {
-        return Promise.reject({status: 400, msg: "Invalid order"})
-    }
-        let sqlQuery= `
+exports.fetchArticles = (sort_by = "created_at", order = "desc") => {
+  const validSorts = [
+    "title",
+    "author",
+    "article_id",
+    "topic",
+    "created_at",
+    "votes",
+    "article_img_url",
+  ];
+  const validOrder = ["asc", "desc"];
+
+  if (!validSorts.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Invalid sort input" });
+  }
+  if (!order || !validOrder.includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order" });
+  }
+  let sqlQuery = `
         SELECT 
         articles.author,
         articles.title,
@@ -36,15 +44,31 @@ exports.fetchArticles = (sort_by='created_at', order='desc') => {
         LEFT JOIN 
         comments ON articles.article_id = comments.article_id
         GROUP BY 
-        articles.article_id `
+        articles.article_id `;
 
-        if(sort_by && validSorts.includes(sort_by)) {
-            sqlQuery += ` ORDER BY ${sort_by} ${order.toLowerCase()}`
-        }
-        console.log(sqlQuery, "<<<< SQL query")
-    return db.query(sqlQuery)
-    .then(({ rows })=> {
-        return rows
+  if (sort_by && validSorts.includes(sort_by)) {
+    sqlQuery += ` ORDER BY ${sort_by} ${order.toLowerCase()}`;
+  }
+
+  return db.query(sqlQuery).then(({ rows }) => {
+    return rows;
+  });
+};
+
+exports.fetchCommentsByArticleId = (article_id) => {
+  if (!article_id) {
+    return Promise.reject({ status: 400, msg: "Bad request" });
+  }
+
+  return db
+    .query(
+      `SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC;`,
+      [article_id]
+    )
+    .then(({ rows }) => {
+      return rows;
     })
-}
-
+    .catch((error) => {
+      throw error;
+    });
+};
