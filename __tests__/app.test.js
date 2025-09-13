29669c0c51294e5da1c508dbcb6ad90831a43b89
CharmingBeetle <nina.e.thomas@gmail.com>
2025-09-13 23:50:36 +0100

    Fix update GitHub Actions workflow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -293,7 +293,7 @@ describe("GET: /api/articles/:article_id", () => {
         expect(article.topic).toBe("mitch");
         expect(article.author).toBe("icellusedkars");
         expect(article.body).toBe("Call me Mitchell. Some years ago..");
-        expect(article.created_at).toBe("2020-10-16T05:03:00.000Z");
+        expect(article.created_at).toBe("2020-10-16T06:03:00.000Z");
         expect(article.votes).toBe(0);
         expect(article.article_img_url).toBe(
           "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
